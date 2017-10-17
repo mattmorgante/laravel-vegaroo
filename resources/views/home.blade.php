90e9b70f2b1ac4cb5d28ee4dc2db49c4e7cf2bc1
Matt Morgante <m.morgante@telegraafmedia.nl>
2017-10-17 21:42:17 +0200

    styling for recipe block on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,76 @@
 @extends('layouts.app')
 
 <div class="container">
-    <h1>Welcome to Vegaroo</h1>
-    <p>Vegaroo has the easiest, cheapest, and most delicious plant-based meals</p>
+    <div class="hero">
+        <h1>Vegaroo</h1>
+        <br>
+        <h2>Easy, Cheap, and Delicious Vegetarian and Vegan meals</h2><h4>Because we could all use a few more fruits and vegetables</h4>
+        <hr>
+    </div>
 
     <h3>What are you in the mood for?</h3>
-    <ul class="categories">
-        <li class="category">Breakfasts</li>
-        <li class="category">Grain Bowls</li>
-        <li class="category">Curries</li>
-        <li class="category">Salads</li>
+    <ul class="flexible_row">
+        <li class="row_item">Breakfasts</li>
+        <li class="row_item">Grain Bowls</li>
+        <li class="row_item">Curries</li>
+        <li class="row_item">Salads</li>
     </ul>
-    <ul class="categories">
-        <li class="category">Stir Fries</li>
-        <li class="category">Snacks</li>
-        <li class="category">Smoothies</li>
-        <li class="category">Classics</li>
+    <ul class="flexible_row">
+        <li class="row_item">Stir Fries</li>
+        <li class="row_item">Snacks</li>
+        <li class="row_item">Smoothies</li>
+        <li class="row_item">Classics</li>
+    </ul>
+
+
+    <h3>Breakfasts</h3>
+    <ul class="flexible_row">
+        <li class="row_item"><a href="/recipes/oatmeal-fruits">Oatmeal & Fruits</a><br>
+            <div class="recipe_extras">
+                <span class="price">$2.45</span>
+                <span class="time">< 5 mins</span>
+                <span class="nutrition_score">7/10</span>
+            </div>
+        </li>
+
+        <li class="row_item"><a href="/recipes/granola-berries">Granola with Berries</a><br>
+            <div class="recipe_extras">
+                <span class="price">$2.45</span>
+                <span class="time">< 5 mins</span>
+                <span class="nutrition_score">4/10</span>
+            </div>
+        </li>
+
+        <li class="row_item"><a href="/recipes/avocado-toast">Avocado on Toast</a><br>
+            <div class="recipe_extras">
+                <span class="price">$2.45</span>
+                <span class="time">< 5 mins</span>
+                <span class="nutrition_score">6/10</span>
+            </div>
+        </li>
+
+        <li class="row_item"><a href="/recipes/protein-smoothie">Oatmeal Protein Smoothie</a><br>
+            <div class="recipe_extras">
+                <span class="price">$2.45</span>
+                <span class="time">< 5 mins</span>
+                <span class="nutrition_score">9/10</span>
+            </div>
+        </li>
     </ul>
 
     <h3>Why plant-based?</h3>
-    <ul class="articles">
-        <li class="article"><a href="/health-benefits">Improve Your Health</a></li>
-        <li class="article"><a href="/environmental-benefits">Prevent Climate Change</a></li>
-        <li class="article"><a href="/stop-animal-cruelty">Stop Animal Cruelty</a></li>
+    <ul class="flexible_row">
+        <li class="row_item"><a href="/health-benefits">Improve Your Health</a></li>
+        <li class="row_item"><a href="/environmental-benefits">Prevent Climate Change</a></li>
+        <li class="row_item"><a href="/stop-animal-cruelty">Stop Animal Cruelty</a></li>
     </ul>
 
-    <h3>Breakfasts</h3>
-    <ul class="meal_carousel">
-        <li>Breakfast #1</li>
-        <li>Breakfast #2</li>
-        <li>Breakfast #3</li>
-        <li>Breakfast #4</li>
+    <h3>Salads</h3>
+    <ul class="flexible_row">
+        <li class="row_item"><a href="/recipes/oatmeal-fruits">Oatmeal & Fruits</a></li>
+        <li class="row_item"><a href="/recipes/granola-berries">Granola with Berries</a></li>
+        <li class="row_item"><a href="/recipes/avocado-toast">Avocado on Toast</a></li>
+        <li class="row_item"><a href="/recipes/protein-smoothie">Oatmeal Protein Smoothie</a></li>
     </ul>
+
 </div>
