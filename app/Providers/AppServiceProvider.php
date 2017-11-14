a9cab8a475e3e8ff17a63766bbc81f9e0e2d7800
Matt Morgante <matthewmorgante@gmail.com>
2017-11-14 12:07:52 +0100

    assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('REDIRECT_HTTPS'))
-        {
-            $url->forceSchema('https');
-        }
     }
 
     /**
