02ae2995a888426b21239842785c5016338837ba
Matt Morgante <matthewmorgante@gmail.com>
2017-11-15 13:35:31 +0100

    trying favicon without public folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,13 @@
     <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Vegaroo</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/public/img/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/public/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/public/img/favicon-16x16.png">
-    <link rel="manifest" href="/public/img/manifest.json">
-    <link rel="mask-icon" href="/public/img/safari-pinned-tab.svg" color="#10cf7d">
-    <link rel="shortcut icon" href="/public/img/favicon.ico">
-    <meta name="msapplication-config" content="/public/img/browserconfig.xml">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="manifest" href="/img/manifest.json">
+    <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#10cf7d">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <meta name="msapplication-config" content="/img/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"/>
     @section('header.javascript')
