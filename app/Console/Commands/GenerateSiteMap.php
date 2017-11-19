3784a2edb2be07a7b0275da28f44cdc9126296f7
Matt Morgante <matthewmorgante@gmail.com>
2017-11-19 14:58:34 +0100

    sitemap generator

---
+++ app/Console/Commands/GenerateSiteMap.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSiteMap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a custom sitemap';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        SitemapGenerator::create('http://vegaroo.co')->writeToFile('public/sitemap-vegaroo.xml');
+
+    }
+}
