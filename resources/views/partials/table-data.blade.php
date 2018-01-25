d15fa2186aff05f1963e187f9c4f01c6132f2a74
Matt Morgante <matthewmorgante@gmail.com>
2018-01-25 22:25:08 +0100

    cleaned up table

--- resources/views/partials/table-data.blade.php
+++ resources/views/partials/table-data.blade.php
@@ -1,6 +1,6 @@
 
 <td class="{{ ($amount >= $recommended) ? 'green' : 'red' }}">
     <i class="fas fa-minus-circle" onclick='increment(-1, "v{{$food}}{{$day->id}}")'></i>
-    <input size=3 id='v{{$food}}{{$day->id}}' name='v' value='{{ $amount }}'>
+    <input class="table-data" disabled size=3 id='v{{$food}}{{$day->id}}' name='v' value='{{ $amount }}'>
     <i class="fas fa-plus-circle" onclick='increment(1, "v{{$food}}{{$day->id}}")'></i>
 </td>
