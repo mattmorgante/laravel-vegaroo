83cc478c51f217d00653f221cb71b48b0f137223
mattmorgante <matthewmorgante@gmail.com>
2018-02-10 21:00:03 +0100

    broken nav

--- resources/views/user-home-2.blade.php
+++ resources/views/user-home-2.blade.php
@@ -242,27 +242,27 @@
     }
 
 
-window.onLoad = function sticky() {
-  var nav = document.getElementById('myProgress');
-  const navTop = nav.offsetTop;
-}
-
-function stickyNavigation() {
-  var nav = document.getElementById('myProgress');
-  const navTop = nav.offsetTop;
-
-  console.log('navTop = ' + navTop);
-  console.log('scrollY = ' + window.scrollY);
-
-  if (window.scrollY >= navTop) {
-    // nav offsetHeight = height of nav
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
-  } else {
-    document.body.style.paddingTop = 0;
-    document.body.classList.remove('fixed-nav');
-  }
-}
+// window.onLoad = function sticky() {
+//   var nav = document.getElementById('myProgress');
+//   const navTop = nav.offsetTop;
+// }
+//
+// function stickyNavigation() {
+//   var nav = document.getElementById('myProgress');
+//   const navTop = nav.offsetTop;
+//
+//   console.log('navTop = ' + navTop);
+//   console.log('scrollY = ' + window.scrollY);
+//
+//   if (window.scrollY >= navTop) {
+//     // nav offsetHeight = height of nav
+//     document.body.style.paddingTop = nav.offsetHeight + 'px';
+//     document.body.classList.add('fixed-nav');
+//   } else {
+//     document.body.style.paddingTop = 0;
+//     document.body.classList.remove('fixed-nav');
+//   }
+// }
 
 window.addEventListener('scroll', stickyNavigation);
 
