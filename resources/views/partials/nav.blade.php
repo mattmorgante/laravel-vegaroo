21d69f5614495ca3eb26297907e7a526fbb7d598
mattmorgante <matthewmorgante@gmail.com>
2018-08-01 18:03:21 +0800

    hamburger menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,17 +4,29 @@
             <a href="/"><img src="/img/logo.png" alt="Vegaroo" class="image_icon"></a>
         </ul>
     </div>
-    <div class="nav-right">
-        <ul>
-            <li><a href="/resources">Resources</a></li>
-            <li><a href="/vegan-recipes">Recipes</a></li>
-            @if ( Auth::guest() )
-                <li class="spacer"></li>
-                <li class="login-nav"><a href="{{ route('login') }}">Login</a></li>
-            @else
-                <li><a href="/home">Dashboard</a></li>
-            @endif
 
-        </ul>
+    <div class="topnav" id="myTopnav">
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">Menu &nbsp;
+            <i class="fa fa-bars"></i>
+        </a>
+        <a href="/resources">Resources</a>
+        <a href="/vegan-recipes">Recipes</a>
+        @if ( Auth::guest() )
+            <a href="{{ route('login') }}">Login</a>
+        @else
+            <a href="/home">Dashboard</a>
+        @endif
     </div>
 </nav>
+
+
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+</script>
