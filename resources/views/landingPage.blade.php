8508e804510eb6b6c911d08e834b9790ad32bb3a
mattmorgante <matthewmorgante@gmail.com>
2018-08-07 15:39:03 +0800

    consistent styling'

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -47,7 +47,8 @@
         </div>
         <div class="flexible_row">
             <ul class="flexible_row">
-                <li class="row_item article_item"><a class="large_title" href="/environmental-benefits">For the planet<img
+                <li class="row_item article_item"><a class="large_title" href="/environmental-benefits">For our
+                        planet<img
                                 class="tiny_img" src="/img/plants_tn.jpg" alt="Plants reduce carbon dioxide in the
                                 atmosphere"></a></li>
                 <li class="row_item article_item"><a class="large_title" href="/health-benefits-long-term">For your health<img
@@ -74,8 +75,22 @@
         <h2 class="title"><a href="/register">Let's Get Started</a></h2>
         <p>Reduce your environmental impact and improve your health starting today</p>
     </div>
-    @include('partials.tools')
+    <div class="flexible_row">
+        <ul class="flexible_row">
+            <li class="row_item article_item"><a class="large_title" href="/dashboard">Track Your Progress With
+                    The Daily
+                    Dozen<img
+                            class="tiny_img" src="/img/veg_tn.jpg" alt="Track
+    Your Progress with the Daily Dozen"></a></li>
+            <li class="row_item article_item"><a class="large_title" href="/calculator">Environmental
+                    Impact Calculator
+                    <img class="tiny_img" src="/img/charts_tn.jpg" alt="Calculate The Environmental Impact Of Your Dieto"></a></li>
+            <li class="row_item article_item"><a class="large_title" href="/small-steps">
+                    <span class="homepage_articles_text">7 Small Steps To Begin Your Vegan Journey</span><br><img
+                            class="tiny_img" src="/img/step_tn.jpg" alt="Taking small steps towards veganism can easily add up over time"></a></li>
+        </ul>
     </div>
+
     <br>
 </div>
 
