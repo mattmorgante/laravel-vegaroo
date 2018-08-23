6590d9130300c67c263b747cfc7db677bb307e92
mattmorgante <matthewmorgante@gmail.com>
2018-08-23 11:20:50 +0800

    new landing page

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -7,11 +7,12 @@
 
 @include('partials.nav')
 
+<div class="homepage_container_white">
 <div class="container">
   <div class="hero homepage-header">
     <div class="wrapper">
-    <h1 class="landing-page-title title" style="margin: 0">Vegaroo Makes It Easier To Eat A <span
-                style="color: #26ce81;">Healthy</span> Plant-Based Diet</h1>
+    <h1 class="landing-page-title title" style="margin: 0">Vegaroo makes it easier to eat a <br><span class="keyword"
+                style="color: #26ce81;">healthy</span><br> plant-based diet</h1>
       <br><br>
         </div>
       <div class="hero">
@@ -19,8 +20,8 @@
               <h3>How does it work?</h3>
               <ul class="hero-list">
                   <li>&#x2705 Track your foods</li>
+                  <li>&#x1F60B Get recipe recommendations</li>
                   <li>&#x1F4C8 Analyze your diet</li>
-                  <li>&#x1f4A1 Get recipe recommendations</li>
                   <li>&#x1F64C Achieve your goals</li>
               </ul>
           </div>
@@ -31,99 +32,125 @@
     <br><br>
 </div>
 
-<div class="homepage_container_grey">
-    <div class="container">
-        <div class="hero">
-            <div class="homepage-flex-wrapper">
-                <div>
-                    <img id="phone-pic" src="/img/phone.png">
-                </div>
-                <div class="text_wrapper">
-                    <h2 class="title">1. Track</h2>
-                    <p>Make sure you are getting enough nutrients</p>
-                </div>
 
-            </div>
+<div class="homepage_container_grey">
+
+    <div class="hero">
+        <h2 class="title">Eat less animal products and you can...</h2>
+    </div>
+
+    <div class="custom-flex-grid">
+        <div class="col homepage_goals">
+            <p>Lose Weight</p>
+        </div>
+        <div class="col homepage_goals">
+            <p>Lower your risk of disease</p>
+        </div>
+
+        <div class="col homepage_goals">
+            <p>Have More Energy</p>
         </div>
     </div>
+    <div class="custom-flex-grid">
+        <div class="col homepage_goals">
+            <p>Protect The Environment</p>
+        </div>
+        <div class="col homepage_goals">
+            <p>Reduce Animal Suffering</p>
+        </div>
+    </div>
+    <br><br>
 </div>
-
 
 <div class="homepage_container_white">
     <div class="container">
-        <div class="hero">
-            <div class="homepage-flex-wrapper">
-                <div class="text_wrapper">
-                    <h2 class="title">2. Analyze</h2>
-                    <p>Find the holes in your diet</p>
-                </div>
+        <br>
+        <div class="quotes">
+            <h4 class="homepage-quote">"But I'm too busy to cook vegan meals!"</h4>
+            <h4 class="homepage-quote">"But I can't find those fancy vegan ingredients at my grocery store!"</h4>
+            <h4 class="homepage-quote">"But vegan food is bland!"</h4>
+            <h4 class="homepage-quote">"But vegan recipes are always so complicated!"</h4>
+        </div>
 
-                <img id="desktop-pic" src="/img/desktop.png">
+        <div class="hero">
+            <div class="hero-list-wrapper">
+                <h3>Vegaroo recipes have a strict no-tolerance policy</h3>
+                <ul class="hero-list">
+                    <li>&#x23F1 <strong>No</strong> recipes longer than 30 minutes</li>
+                    <li>&#x1F644 <strong>No</strong> fancy ingredients</li>
+                    <li>&#x1F35A <strong>No</strong> tasteless meals</li>
+                    <li>&#x1F914 <strong>No</strong> complicated cooking techniques</li>
+                    <li>&#x1F346 <strong>No</strong> food porn</li>
+                </ul>
             </div>
         </div>
     </div>
+    <br><br>
 </div>
 
-
 <div class="homepage_container_grey">
-    <div class="container">
-        <div class="hero">
-            <h2 class="title">3. Get Personalized Recipe Recommendations
-                    </h2>
-            <h3>All Vegaroo recipes are easy, cheap, nutritious and delicious</h3>
-        </div>
-        <ul class="flexible_row">
-            @foreach ($recipes as $recipe)
+   <div class="container">
+       <div class="features-wrapper">
+           <div>
+               <h2>&#x2705 Track</h2>
+               <p>Make sure you eat enough nutrients</p>
+           </div>
+           <div class="photo">
+               <img id="phone-pic" src="/img/iphone-3.png">
+           </div>
+       </div>
+   </div>
+</div>
+
+    <div class="homepage_container_white">
+        <div class="container">
+            <div class="hero">
+                <h2 class="title">&#x1F60B Eat</h2>
+                <h3>Get personalized recipe recommendations like these favorites</h3>
+            </div>
+            <ul class="flexible_row">
+                @foreach ($recipes as $recipe)
                 @include('partials.recipe-box')
-            @endforeach
-        </ul>
-        <div class="read-more">
-            <a href="/vegan-recipes">All Recipes<i style="margin-left: 6px;" class="fas fa-arrow-right"></i></a>
+                @endforeach
+            </ul>
+            <div class="read-more">
+                <a href="/vegan-recipes">All Recipes<i style="margin-left: 6px;" class="fas fa-arrow-right"></i></a>
+            </div>
         </div>
+        <br>
     </div>
-    <br>
+
+<div class="homepage_container_grey">
+    <div class="features-wrapper">
+    <div>
+        <h2>&#x1F4C8 Improve</h2>
+        <p>Find and plug the holes in your diet</p>
+    </div>
+    <img id="desktop-pic" src="/img/desktop.png">
+
+    </div>
+    <div class="btn-wrapper">
+        <a class="btn" href="/register">Get Started For Free</a>
+    </div>
+    <br><br>
 </div>
 
-<div class="homepage_container_white">
-    <div class="container">
-        <div class="hero">
-            <h2 class="title">4. Achieve your goals</h2>
-            <h3>Why do you want to go Vegan?</h3>
-        </div>
 
-        <div class="custom-flex-grid">
-            <div class="col homepage_goals" id="box-1" onclick="selectBox(1)">
-                <p>Lose Weight</p>
-            </div>
-            <div class="col homepage_goals" id="box-1" onclick="selectBox(1)">
-                <p>Save The Environment</p>
-            </div>
-            <div class="col homepage_goals" id="box-3" onclick="selectBox(3)">
-                <p>Have More Energy</p>
-            </div>
-        </div>
-        <div class="custom-flex-grid">
-            <div class="col homepage_goals" id="box-1" onclick="selectBox(1)">
-                <p>Reduce Animal Suffering</p>
-            </div>
-            <div class="col homepage_goals" id="box-3" onclick="selectBox(3)">
-                <p>Lose Body Fat</p>
-            </div>
-        </div>
 
-        <div class="btn-wrapper">
-            <a class="btn" href="/register">Get Started</a>
-        </div>
-        <br><br>
-    </div>
-</div>
 
 @endsection
 
 <script>
-//
-//    function selectBox(box) {
-//        var element = document.getElementById('box-' + box);
-//        element.style.backgroundColor = "#26ce81";
-//    }
+    var keywords = ["healthy", "sustainable", "ethical", "cheap"];
+    var colours = ["#26ce81", "#26ce81", "#26ce81", "#26ce81"];
+    var count = 1;
+    setInterval(function(){
+        $("span.keyword").fadeOut(400, function(){
+            $(this).html(keywords[count]).css("color", colours[count]);
+            count++;
+            if(count === keywords.length)
+                count = 0;
+            $(this).fadeIn(400);
+        });
+    }, 2000);
 </script>
