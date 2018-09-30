19f994736986e65fc189c64d7b7f8a4d17ba9886
mattmorgante <matthewmorgante@gmail.com>
2018-09-30 16:57:05 +0200

    change tests

--- tests/Feature/BasicTests.php
+++ tests/Feature/BasicTests.php
@@ -3,15 +3,9 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BasicTests extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
         $response = $this->get('/');
