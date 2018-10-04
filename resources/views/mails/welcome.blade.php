fc04d806b92a8143a25ab7787e31931f73d3ac8d
mattmorgante <matthewmorgante@gmail.com>
2018-10-04 16:42:15 +0200

    text for welcome email

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -1,8 +1,19 @@
-Hey there <i>{{ $mailInfo->receiver }}</i>,
+Hey there {{ $mailInfo->receiver }},
 <h2>Welcome To Vegaroo!</h2>
 
-<p><a href="https://vegaroo.co/?ref=email">Vegaroo</a></p>
+<p>We're thrilled to have you on board. Vegaroo is a website to help people consume fewer animal products. Here are just a few of things you can expect to find on Vegaroo</p>
 
-Happy
-<br/>
+<h4>Nutrient Tracking</h4>
+<p>It can be tough to get enough nutrients on a Vegan diet. Vegaroo has you covered with a comprehensive dashboard to keep track of the 12 most important food groups for aspiring vegans. As a member of Vegaroo, you can track your progress, find ways to improve, and get personalized recipe recommendations. Just visit <a href="https://www.vegaroo.co/home/?ref=email">Your Dashboard to get started.</p>
+
+<h4>Recipes</h4>
+<p>Vegaroo has a database of more than 25 Vegan recipes you can cook in less than 30 minutes with completely normal ingredients and no complicated cooking techniques. For some inspiration for tonight's dinner, check out our <a href="https://vegaroo.co/vegan-recipes/?ref=email">Recipes Page.</a></p>
+
+<h4>Resources</h4>
+<p>Not sure how to get started with a plant-based diet? Looking for some help <a href="https://www.vegaroo.co/resources/?ref=email">Resources Page.</a></p>
+
+<p>Happy Eating!</p>
 {{ $mailInfo->sender }}
+
+<br>
+P.S. Sick of Vegaroo? Unsubscribe at any time by just replying "unsubscribe" to this e-mail ;)
--- resources/views/mails/welcome_plain.blade.php
+++ resources/views/mails/welcome_plain.blade.php
@@ -1,6 +1,22 @@
-Hello {{ $mailInfo->receiver }},
-This is a demo email for testing purposes! Also, it's the plain-text
+Hey there {{ $mailInfo->receiver }},
 
+Welcome To Vegaroo!
 
-Thank You,
+We're thrilled to have you on board. Vegaroo is a website to help people consume fewer animal products. Here are just a few of things you can expect to find on Vegaroo.
+
+Nutrient Tracking
+It can be tough to get enough nutrients on a Vegan diet. Vegaroo has you covered with a comprehensive dashboard to keep track of the 12 most important food groups for aspiring vegans. As a member of Vegaroo, you can track your progress, find ways to improve, and get personalized recipe recommendations. Just visit Your Dashboard to get started.
+https://www.vegaroo.co/home
+
+Recipes
+Vegaroo has a database of more than 25 Vegan recipes you can cook in less than 30 minutes with completely normal ingredients and no complicated cooking techniques. To get started, check out our Recipes Page.
+https://www.vegaroo.co/vegan-recipes
+
+Resources
+Not sure how to get started with a plant-based diet? Looking for some help Resources Page.
+https://www.vegaroo.co/resources
+
+Happy Eating!
 {{ $mailInfo->sender }}
+
+P.S. Sick of Vegaroo? Unsubscribe at any time by just replying "unsubscribe" to this e-mail ;)
