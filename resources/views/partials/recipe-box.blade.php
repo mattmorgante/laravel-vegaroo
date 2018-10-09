b117d2e31d64b218646e97830f23f630f9fe3aa9
mattmorgante <matthewmorgante@gmail.com>
2018-10-09 13:27:37 +0200

    remove unnecessary labels

--- resources/views/partials/recipe-box.blade.php
+++ resources/views/partials/recipe-box.blade.php
@@ -1,6 +1,10 @@
 <li class="row_item"><a class="article_link" href="/vegan-recipes/{{ $recipe->category }}/{{ $recipe->slug }}">{{ $recipe->title }}</a><br>
     <div class="recipe_extras">
-        <span class="price">Calories: {{ $recipe->calories }}</span><br>
-        <span class="time">Time: {{ $recipe->time }}</span>
+        @if($recipe->calories == 'It varies')
+            <span class="price">Calories will vary</span><br>
+        @else
+            <span class="price">{{ $recipe->calories }} Calories</span><br>
+        @endif
+        <span class="time">{{ $recipe->time }}</span>
     </div>
 </li>
